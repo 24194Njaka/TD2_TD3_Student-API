5c2bfd85cfaf242a2a319575a6e3dec72958715b
Njaka <hei.herinjaka@gmail.com>
2026-03-17 21:45:41 +0300

    fix: change reference type from int to String and add @NoArgsConstructor in Student entity

--- src/main/java/com/td2/student_api/entity/Student.java
+++ src/main/java/com/td2/student_api/entity/Student.java
@@ -3,13 +3,15 @@ package com.td2.student_api.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class Student {
-    private int reference;
+    private String reference;
     private String firstName;
     private String lastName;
     private int age;
