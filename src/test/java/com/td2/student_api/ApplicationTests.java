b05b6e98ef6ba6213c24eb58ee9c86a4de6457f7
Njaka <hei.herinjaka@gmail.com>
2026-03-18 18:36:05 +0300

    Initial commit

---
+++ src/test/java/com/td2/student_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td2.student_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
